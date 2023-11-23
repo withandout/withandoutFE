c1c92c1722abfcf6ad6ae6f21823d81bd0af2e0d
Lainlnya <kimyelim100@gmail.com>
2023-11-23 23:43:04 +0900

    fix: Lainlnya fixed party apply page

--- withandoutFE/src/views/JoinPartyView.vue
+++ withandoutFE/src/views/JoinPartyView.vue
@@ -1,27 +1,36 @@
 <template>
   <section class="partyList" :class="{ disable: isApplied }">
     <section class="parties">
-      <section
-        class="eachParties"
-        v-for="party in store.partyList"
-        :key="party.partyNo"
-      >
-        <!-- <div
-          class="partyHidden"
-          :class="{ partyChecked: selectedPartyNo === party.partyNo }"
+      <template v-if="parties.length > 0">
+        <section
+          class="eachParties"
+          v-for="party in store.partyList"
+          :key="party.partyNo"
         >
-          ✓
-        </div> -->
-        <Party
-          :party="party"
-          @click="selectedParty(party.partyNo)"
-          :class="{ partyFocus: selectedPartyNo === party.partyNo }"
-        />
-      </section>
+          <!-- <div
+            class="partyHidden"
+            :class="{ partyChecked: selectedPartyNo === party.partyNo }"
+          >
+            ✓
+          </div> -->
+          <Party
+            :party="party"
+            @click="selectedParty(party.partyNo)"
+            :class="{ partyFocus: selectedPartyNo === party.partyNo }"
+          />
+        </section>
+      </template>
+      <h1 v-else>참여할 수 있는 파티가 없습니다.</h1>
     </section>
     <section class="partyInfo">
-      <PartyInfo :clickedParty="clickedParty" />
-      <LeaderNameCard :leader="leader" />
+      <template v-if="parties.length > 0">
+        <PartyInfo :clickedParty="clickedParty" />
+      </template>
+      <h1 v-else>파티 정보가 없습니다.</h1>
+      <template v-if="parties.length > 0">
+        <LeaderNameCard :leader="leader" />
+      </template>
+      <h1 v-else>리더가 없습니다.</h1>
       <section class="partyBtns">
         <button @click.prevent="isApplied = true">신청하기</button>
         <button @click.prevent="() => router.push({ name: 'home' })">
@@ -58,7 +67,7 @@
             class="applyBtn"
             @click.prevent="
               applyToParty(
-                store.partyList[clickedPartyNum].partyNo,
+                selectedPartyNo,
                 userInfo.userNo,
                 Date.now(),
                 introduce
@@ -90,35 +99,44 @@ const router = useRouter();
 const clickedParty = ref({});
 const selectedPartyNo = ref(0);
 const leader = ref({});
+const parties = ref([]);
 
 // clickListReset, clickedPartyNo 초기화.
-const clickListReset = () => {
-  clickedPartyNum.value = 0;
+const clickListReset = (partyNo) => {
   introduce.value = '';
+  parties.value = parties.value.filter((party) => party.partyNo !== partyNo);
+
+  // selectedPartyNo.value = parties.value[0].partyNo;
+
+  store.selectParty(parties.value[0].partyNo).then((data) => {
+    clickedParty.value = data;
+    selectedPartyNo.value = data.partyNo;
+    store.selectLeader(selectedPartyNo.value).then((data) => {
+      leader.value = data;
+    });
+  });
 };
 
-const clickedPartyNum = ref(0);
 const userInfo = ref(JSON.parse(sessionStorage.getItem('sessionId')));
 const isApplied = ref(false);
+
 const applyToParty = (partyNo, userNo, Date, introduce) => {
   store.applyToParty(partyNo, userNo, Date, introduce);
   isApplied.value = false;
-  clickListReset();
+  clickListReset(partyNo);
 };
 
 onMounted(() => {
   store
     .selectAvailableParty(userInfo.value.userNo, userInfo.value.region)
     .then((data) => {
+      parties.value = data;
       selectedPartyNo.value = data[0].partyNo;
       clickedParty.value = data[0];
       store.selectLeader(selectedPartyNo.value).then((data) => {
         leader.value = data;
       });
     });
-
-  // 토글 초기화
-  clickListReset();
 });
 
 // 파티 선택하는 함수
--- withandoutFE/src/views/LoginView.vue
+++ withandoutFE/src/views/LoginView.vue
@@ -1,21 +1,24 @@
 <template>
   <form class="loginForm">
-    <label for="id">아이디</label>
-    <input type="text" id="id" v-model="userId" tabindex="1" />
-    <label for="password">비밀번호</label>
-    <input type="password" id="password" v-model="password" tabindex="2" />
-    <section class="btns">
-      <button class="loginBtn" @click.prevent="loginUser" tabindex="3">
-        로그인
-      </button>
-      <button
-        class="signupBtn"
-        @click.prevent="() => router.push({ name: 'signup' })"
-        tabindex="-1"
-      >
-        회원가입
-      </button>
-    </section>
+    <div class="loginBackground"></div>
+    <div class="loginSec">
+      <label for="id">아이디</label>
+      <input type="text" id="id" v-model="userId" tabindex="1" />
+      <label for="password">비밀번호</label>
+      <input type="password" id="password" v-model="password" tabindex="2" />
+      <section class="btns">
+        <button class="loginBtn" @click.prevent="loginUser" tabindex="3">
+          로그인
+        </button>
+        <button
+          class="signupBtn"
+          @click.prevent="() => router.push({ name: 'signup' })"
+          tabindex="-1"
+        >
+          회원가입
+        </button>
+      </section>
+    </div>
   </form>
 </template>
 
